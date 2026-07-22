2d4bd5f7edc15eb0f8855e32906f0a1c40d8ff77
Dharani Bhartu <serein.reverie.meraki@gmail.com>
2026-07-22 17:30:28 +0530

    Complete Phase 3: Authentication with JWT and MySQL

--- server/app.js
+++ server/app.js
@@ -5,16 +5,28 @@ const cors = require("cors");
 
 require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+const verifyToken = require("./middleware/authMiddleware");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("CareerPilot API Running...");
 });
 
 const PORT = process.env.PORT || 5000;
+
+app.get("/api/profile", verifyToken, (req, res) => {
+  res.json({
+    message: "Protected Route Accessed Successfully",
+    user: req.user,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
