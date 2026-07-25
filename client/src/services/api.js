b6e376f74546876ab3396814dfaccea11f532e1b
Dharani Bhartu <serein.reverie.meraki@gmail.com>
2026-07-25 12:15:39 +0530

    fix: update production api base url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://careerpilot-backend-eks1.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
