9d46a48282472a70bb361334267fbc8ba29b7298
Dharani Bhartu <serein.reverie.meraki@gmail.com>
2026-07-25 10:24:09 +0530

    Configure Aiven Mysql

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,15 @@
 const mysql = require("mysql2");
 require("dotenv").config();
-console.log("HOST:", process.env.DB_HOST);
-console.log("USER:", process.env.DB_USER);
-console.log("PASSWORD:", process.env.DB_PASSWORD);
-console.log("DATABASE:", process.env.DB_NAME);
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 connection.connect((err) => {
